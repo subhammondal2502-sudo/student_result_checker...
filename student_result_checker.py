dbe526b843d09067e87f72457d1e3645e898c7ae
Subham Mondal <subhammondal2502@gmail.com>
2026-09-10 18:52:02 +0530

    Update student_result_checker.py

--- student_result_checker.py
+++ student_result_checker.py
@@ -9,3 +9,12 @@ average=(total_marks)/3
 print("average marks :" , average)
 if math_marks<30 or physics_marks<30 or computer_marks<30 :
     print("result : fail")  
+else:         
+    if(average>=60):
+        print("result : excellent")
+    elif(average>=50):
+        print("result : very good")
+    elif(average>=40):
+        print("result : good")
+    elif(average>=30):
+        print("result : pass")
