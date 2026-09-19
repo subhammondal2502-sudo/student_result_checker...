86f7cf11c00f3972912d83992b998fe65b55bde0
Subham Mondal <subhammondal2502@gmail.com>
2026-09-19 20:28:09 +0530

    Add English marks input .

--- student_result_checker.py
+++ student_result_checker.py
@@ -1,6 +1,7 @@
 #Q.1 student result checker .....
 name=(input("enter your name :"))
 bengali=int(input("enter marks in bengali :"))
+english=int(input("enter marks in english :"))
 math_marks=int(input("marks in mathematics :"))
 physics_marks=int(input("marks in physics :"))
 computer_marks=int(input("marks in computer science :"))
