ab88f2ea84b2c47057bce5254e515dca2e5853bc
Subham Mondal <subhammondal2502@gmail.com>
2026-09-19 20:29:34 +0530

    Update student_result_checker.py

--- student_result_checker.py
+++ student_result_checker.py
@@ -2,7 +2,7 @@
 name=(input("enter your name :"))
 bengali=int(input("enter marks in bengali :"))
 english=int(input("enter marks in english :"))
-math_marks=int(input("marks in mathematics :"))
+mathematics=int(input("enter marks in mathematics :"))
 physics_marks=int(input("marks in physics :"))
 computer_marks=int(input("marks in computer science :"))
 total_marks = (math_marks+physics_marks+computer_marks)
