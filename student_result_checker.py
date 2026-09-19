f8d86bb3ec4f9d32810564200f8079445ac7f4fd
Subham Mondal <subhammondal2502@gmail.com>
2026-09-19 21:07:41 +0530

    Add marks validation for all subjects .

--- student_result_checker.py
+++ student_result_checker.py
@@ -7,6 +7,8 @@ science=int(input("enter marks in physics :"))
 computer=int(input("enter marks in computer :"))
 total_marks = (bengali + english + mathematics + science + computer)
 average=float(total_marks / 5)
+if bengali<0 or bengali>100 or english<0 or english>100 or mathematics<0 or mathematics>100 or science<0 or science>100 or computer<0 or computer>100 :
+    print("invalid marks")
 print("average marks :" , average)
 if math_marks<30 or physics_marks<30 or computer_marks<30 :
     print("result : fail")  
