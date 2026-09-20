164df926b2f8cab6afe1b429c1786a944cee305f
Subham Mondal <subhammondal2502@gmail.com>
2026-09-20 07:26:44 +0530

    Update student_result_checker.py

--- student_result_checker.py
+++ student_result_checker.py
@@ -10,7 +10,7 @@ average=float(total_marks / 5)
 if bengali<0 or bengali>100 or english<0 or english>100 or mathematics<0 or mathematics>100 or science<0 or science>100 or computer<0 or computer>100 :
     print("invalid marks")
 else:
-if math_marks<30 or physics_marks<30 or computer_marks<30 :
+    if math_marks<30 or physics_marks<30 or computer_marks<30 :
     print("result : fail")  
 else:         
     if(average>=60):
