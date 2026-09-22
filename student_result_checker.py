103d9bd19d08903f71ba6fc888590d60ef418f0a
Subham Mondal <subhammondal2502@gmail.com>
2026-09-22 05:38:38 +0530

    Update student_result_checker.py

--- student_result_checker.py
+++ student_result_checker.py
@@ -20,8 +20,8 @@ else:
         if(average>=90):
             print("grade : A+")
         
-    elif(average>=80):
-        print("grade : A")
+        elif(average>=80):
+            print("grade : A")
         
     elif(average>=70):
         print("grade : B+")
