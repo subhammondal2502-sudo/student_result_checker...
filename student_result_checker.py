5bf251cc4db16661980c72805fc93aa4ea521f09
Subham Mondal <subhammondal2502@gmail.com>
2026-09-22 05:36:34 +0530

    Update student_result_checker.py

--- student_result_checker.py
+++ student_result_checker.py
@@ -29,7 +29,7 @@ else:
         
     elif(average>=60):
         print("grade : B")
-    elif (average>=50):
+    elif(average>=50):
         print("grade : C")
     elif(average>=33):
         print("grade : D")
